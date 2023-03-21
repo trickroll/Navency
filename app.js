e29bebaa1da20bf871ed8d9923ce4ca63863fc34
Glitch (messenger-platform-quick-start) <none>
2023-03-21 05:23:42 +0000

    🚊🐔 Checkpoint ./app.js:56099588/13360

--- app.js
+++ app.js
@@ -13,14 +13,12 @@
  *
  */
 
-'use strict';
-
+"use strict";
 
 // Imports dependencies and set up http server
-const
-  request = require('request'),
-  express = require('express'),
-  { urlencoded, json } = require('body-parser'),
+const request = require("request"),
+  express = require("express"),
+  { urlencoded, json } = require("body-parser"),
   app = express();
 
 // Parse application/x-www-form-urlencoded
@@ -30,31 +28,27 @@ app.use(urlencoded({ extended: true }));
 app.use(json());
 
 // Respond with 'Hello World' when a GET request is made to the homepage
-app.get('/', function (_req, res) {
-  res.send('Hello World');
+app.get("/", function (_req, res) {
+  res.send("Hello World");
 });
 
 // Adds support for GET requests to our webhook
-app.get('/webhook', (req, res) => {
-
+app.get("/webhook", (req, res) => {
   // Your verify token. Should be a random string.
   const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
   // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
 
   // Checks if a token and mode is in the query string of the request
   if (mode && token) {
-
     // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
       // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
+      console.log("WEBHOOK_VERIFIED");
       res.status(200).send(challenge);
-
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
       res.sendStatus(403);
@@ -63,22 +57,20 @@ app.get('/webhook', (req, res) => {
 });
 
 // Creates the endpoint for your webhook
-app.post('/webhook', (req, res) => {
+app.post("/webhook", (req, res) => {
   let body = req.body;
 
   // Checks if this is an event from a page subscription
-  if (body.object === 'page') {
-
+  if (body.object === "page") {
     // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-
+    body.entry.forEach(function (entry) {
       // Gets the body of the webhook event
       let webhookEvent = entry.messaging[0];
       console.log(webhookEvent);
 
       // Get the sender PSID
       let senderPsid = webhookEvent.sender.id;
-      console.log('Sender PSID: ' + senderPsid);
+      console.log("Sender PSID: " + senderPsid);
 
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
@@ -90,9 +82,8 @@ app.post('/webhook', (req, res) => {
     });
 
     // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
+    res.status(200).send("EVENT_RECEIVED");
   } else {
-
     // Returns a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
@@ -107,36 +98,37 @@ function handleMessage(senderPsid, receivedMessage) {
     // Create the payload for a basic text message, which
     // will be added to the body of your request to the Send API
     response = {
-      'text': `You sent the message: '${receivedMessage.text}'. Now send me an attachment!`
+      text: `You sent the message: '${receivedMessage.text}'. Now send me an attachment!`,
     };
   } else if (receivedMessage.attachments) {
-
     // Get the URL of the message attachment
     let attachmentUrl = receivedMessage.attachments[0].payload.url;
     response = {
-      'attachment': {
-        'type': 'template',
-        'payload': {
-          'template_type': 'generic',
-          'elements': [{
-            'title': 'Is this the right picture?',
-            'subtitle': 'Tap a button to answer.',
-            'image_url': attachmentUrl,
-            'buttons': [
-              {
-                'type': 'postback',
-                'title': 'Yes!',
-                'payload': 'yes',
-              },
-              {
-                'type': 'postback',
-                'title': 'No!',
-                'payload': 'no',
-              }
-            ],
-          }]
-        }
-      }
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [
+            {
+              title: "Is this the right picture?",
+              subtitle: "Tap a button to answer.",
+              image_url: attachmentUrl,
+              buttons: [
+                {
+                  type: "postback",
+                  title: "Yes!",
+                  payload: "yes",
+                },
+                {
+                  type: "postback",
+                  title: "No!",
+                  payload: "no",
+                },
+              ],
+            },
+          ],
+        },
+      },
     };
   }
 
@@ -153,10 +145,10 @@ function handlePostback(senderPsid, receivedPostback) {
   let payload = receivedPostback.payload;
 
   // Set the response based on the postback payload
-  if (payload === 'yes') {
-    response = { 'text': 'Thanks!' };
-  } else if (payload === 'no') {
-    response = { 'text': 'Oops, try sending another image.' };
+  if (payload === "yes") {
+    response = { text: "Thanks!" };
+  } else if (payload === "no") {
+    response = { text: "Oops, try sending another image." };
   }
   // Send the message to acknowledge the postback
   callSendAPI(senderPsid, response);
@@ -164,73 +156,78 @@ function handlePostback(senderPsid, receivedPostback) {
 
 // Sends response messages via the Send API
 function callSendAPI(senderPsid, response) {
-
   // The page access token we have generated in your app settings
   const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
   // Construct the message body
   let requestBody = {
-    'recipient': {
-      'id': senderPsid
+    recipient: {
+      id: senderPsid,
     },
-    'message': response
+    message: response,
   };
 
   // Send the HTTP request to the Messenger Platform
-  request({
-    'uri': 'https://graph.facebook.com/v2.6/me/messages',
-    'qs': { 'access_token': PAGE_ACCESS_TOKEN },
-    'method': 'POST',
-    'json': requestBody
-  }, (err, _res, _body) => {
-    if (!err) {
-      console.log('Message sent!');
-    } else {
-      console.error('Unable to send message:' + err);
+  request(
+    {
+      uri: "https://graph.facebook.com/v2.6/me/messages",
+      qs: { access_token: PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: requestBody,
+    },
+    (err, _res, _body) => {
+      if (!err) {
+        console.log("Message sent!");
+      } else {
+        console.error("Unable to send message:" + err);
+      }
     }
-  });
+  );
 }
 
 // Sends opt-in request
-function sendOptInRequest(senderPsid, response){
-  
+function sendOptInRequest(senderPsid, response) {
   // The page access token we have generated in your app settings
   const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-  
+
   // Construct the message body
   let requestBody = {
-    'recipient': {
-      'id': senderPsid
+    recipient: {
+      id: senderPsid,
     },
-    'message': {
-    "attachment":{
-      "type":"template", 
-      "payload":{
-         "template_type":"notification_messages", 
-          "title":"test-optin",
-          "image_url":"https://picsum.photos/200",
-          "payload": "promotional",
-      }
-    }
-  }
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "notification_messages",
+          title: "test-optin",
+          image_aspect_ratio: "SQUARE",
+          notification_messages_reoptin: "ENABLED",
+          image_url: "https://picsum.photos/200",
+          payload: "promotional",
+        },
+      },
+    },
   };
   // Send the HTTP request to the Messenger Platform
-  request({
-    'uri': 'https://graph.facebook.com/v16.0/106030262429449/messages',
-    'qs': { 'access_token': PAGE_ACCESS_TOKEN },
-    'method': 'POST',
-    'json': requestBody
-  }, (err, _res, _body) => {
-    if (!err) {
-      console.log('optin sent!');
-    } else {
-      console.error('Unable to send optin:' + err);
+  request(
+    {
+      uri: "https://graph.facebook.com/v16.0/106030262429449/messages",
+      qs: { access_token: PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: requestBody,
+    },
+    (err, _res, _body) => {
+      if (!err) {
+        console.log("optin sent!");
+      } else {
+        console.error("Unable to send optin:" + err);
+      }
     }
-  });
-  };
-
+  );
+}
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
 });
