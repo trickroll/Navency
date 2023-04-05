fc88293d34391f20117bfcb074254a6951490544
Glitch (broken-pointy-chicken) <none>
2023-04-05 21:47:20 +0800

    comment out other getuserprofile

--- graph-api-new.js
+++ graph-api-new.js
@@ -273,26 +273,26 @@ module.exports = class GraphApiNew {
     }
   }
 
-  async getUserProfile(senderIgsid) {
-    let url = new URL(`${config.apiUrl}/${senderIgsid}`);
-    url.search = new URLSearchParams({
-      access_token: this.pageAccesToken,
-      fields: "first_name, last_name, profile_pic",
-    });
-    let response = await fetch(url);
-    if (response.ok) {
-      let userProfile = await response.json();
-      return {
-        firstName: userProfile.first_name,
-        lastName: userProfile.last_name,
-        profilePic: userProfile.profile_pic,
-      };
-    } else {
-      console.warn(
-        `Could not load profile for ${senderIgsid}: ${response.statusText}`,
-        await response.json()
-      );
-      return null;
-    }
-  }
+  // async getUserProfile(senderIgsid) {
+  //   let url = new URL(`${config.apiUrl}/${senderIgsid}`);
+  //   url.search = new URLSearchParams({
+  //     access_token: this.pageAccesToken,
+  //     fields: "first_name, last_name, profile_pic",
+  //   });
+  //   let response = await fetch(url);
+  //   if (response.ok) {
+  //     let userProfile = await response.json();
+  //     return {
+  //       firstName: userProfile.first_name,
+  //       lastName: userProfile.last_name,
+  //       profilePic: userProfile.profile_pic,
+  //     };
+  //   } else {
+  //     console.warn(
+  //       `Could not load profile for ${senderIgsid}: ${response.statusText}`,
+  //       await response.json()
+  //     );
+  //     return null;
+  //   }
+  // }
 };
