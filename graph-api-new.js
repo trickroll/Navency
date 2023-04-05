c91c3e2d54239360cba98f66273529abd682f208
Glitch (broken-pointy-chicken) <none>
2023-04-05 21:51:36 +0800

    wrong userprof

--- graph-api-new.js
+++ graph-api-new.js
@@ -247,6 +247,29 @@ module.exports = class GraphApiNew {
     }
   }
   static async getUserProfile(senderIgsid) {
+    let url = new URL(`${config.apiUrl}/${senderIgsid}`);
+    url.search = new URLSearchParams({
+      access_token: this.pageAccesToken,
+      fields: "first_name, last_name, profile_pic",
+    });
+    let response = await fetch(url);
+    if (response.ok) {
+      let userProfile = await response.json();
+      return {
+        firstName: userProfile.first_name,
+        lastName: userProfile.last_name,
+        profilePic: userProfile.profile_pic,
+      };
+    } else {
+      console.warn(
+        `Could not load profile for ${senderIgsid}: ${response.statusText}`,
+        await response.json()
+      );
+      return null;
+    }
+  }
+
+  async getUserProfile(senderIgsid) {
     console.log(`${config.apiUrl}/${senderIgsid}`)
     let url = new URL(`${config.apiUrl}/${senderIgsid}`);
     url.search = new URLSearchParams({
@@ -272,27 +295,4 @@ module.exports = class GraphApiNew {
       return null;
     }
   }
-
-  // async getUserProfile(senderIgsid) {
-  //   let url = new URL(`${config.apiUrl}/${senderIgsid}`);
-  //   url.search = new URLSearchParams({
-  //     access_token: this.pageAccesToken,
-  //     fields: "first_name, last_name, profile_pic",
-  //   });
-  //   let response = await fetch(url);
-  //   if (response.ok) {
-  //     let userProfile = await response.json();
-  //     return {
-  //       firstName: userProfile.first_name,
-  //       lastName: userProfile.last_name,
-  //       profilePic: userProfile.profile_pic,
-  //     };
-  //   } else {
-  //     console.warn(
-  //       `Could not load profile for ${senderIgsid}: ${response.statusText}`,
-  //       await response.json()
-  //     );
-  //     return null;
-  //   }
-  // }
 };
